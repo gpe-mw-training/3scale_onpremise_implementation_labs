06a44ff05e528626197b5aced96f8554059f6004
jbride <jbride2001@yahoo.com>
2017-05-22 10:47:42 -0600

    more updates related to SSO security of swarm service

--- services/wf_swarm_datestamp_service/src/main/java/org/wildfly/swarm/examples/jaxrs/swagger/TimeResource.java
+++ services/wf_swarm_datestamp_service/src/main/java/org/wildfly/swarm/examples/jaxrs/swagger/TimeResource.java
@@ -1,9 +1,13 @@
 package org.wildfly.swarm.examples.jaxrs.swagger;
 
+import java.security.Principal;
+import javax.servlet.http.HttpServletRequest;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Context;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -21,7 +25,16 @@ public class TimeResource {
             response = String.class
     )
     @Produces(MediaType.APPLICATION_JSON)
-    public String get() {
+    public String get(@Context HttpServletRequest request) {
+
+        Principal principal = request.getUserPrincipal();
+        if(principal != null) {
+            String pName = principal.getName();
+            System.out.println("get() pName = "+pName+" : principal class type = "+principal.getClass().toString());
+        }else {
+            System.out.println("get() principal not found ");
+        }
+
         return String.format("{\"value\" : \"The time is %s\"}", new DateTime());
     }
 }
