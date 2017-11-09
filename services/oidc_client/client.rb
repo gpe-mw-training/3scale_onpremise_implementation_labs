52be75dba568969e0a772c3bb1dd92534ba715cc
jbride <jbride2001@yahoo.com>
2017-11-09 14:20:14 -0700

    latest

--- services/oidc_client/client.rb
+++ services/oidc_client/client.rb
@@ -4,12 +4,12 @@ require 'securerandom'
 enable :sessions
 set :session_secret, '*&(^B234'
 
-GATEWAY = ENV['GATEWAY'] || "http://localhost:8080"
+IDP_URL = ENV['IDP_URL'] || "http://localhost:8080"
 CLIENT_ID = ENV['CLIENT_ID']
 CLIENT_SECRET = ENV['CLIENT_SECRET']
 REDIRECT_URI = ENV['REDIRECT_URI'] || "http://localhost:3001/callback"
-AUTHORIZE_ENDPOINT = "#{GATEWAY}/authorize"
-TOKEN_ENDPOINT = "#{GATEWAY}/oauth/token"
+AUTHORIZE_ENDPOINT = "#{IDP_URL}/authorize"
+TOKEN_ENDPOINT = "#{IDP_URL}/oauth/token"
 
 get("/") do
   @state = SecureRandom.uuid
