6de67f97189b8468a525c611ffb016209c4e09f8
jbride <jbride2001@yahoo.com>
2018-08-01 13:24:03 -0600

    update to create_tenants.sh

--- util/create_tenants.sh
+++ util/create_tenants.sh
@@ -1,12 +1,13 @@
 
-startTenant=2
-endTenant=3
+startTenant=1
+endTenant=1
 
 master_access_token=ga7f0bqe
 OCP_WILDCARD_DOMAIN=$OCP_WILDCARD_DOMAIN
 tenant_passwd=admin
 create_tenant_url=https://$OCP_PROJECT_PREFIX-master-admin.$OCP_WILDCARD_DOMAIN/master/api/providers.xml
 output_dir=/tmp/3scale_tenant
+access_token_mapping_file=$output_dir/access_token_mapping_file.txt
 
 function createAndActivateTenants() {
 
@@ -14,28 +15,47 @@ function createAndActivateTenants() {
     
 
     for i in $(seq ${startTenant} ${endTenant}) ; do
-        orgName=acme$i;
+        orgName=user$i-3scale;
         userId=user$i;
-        email=user$1@$OCP_WILDCARD_DOMAIN
+        output_file=$orgName-tenant-signup.xml
   
        # 1) Create tenant 
-       curl -v  \
-            -k \
+       curl -k  \
             -X POST \
             -d access_token=$master_access_token \
             -d org_name=$orgName \
             -d username=$userId \
             -d email=$userId%40$OCP_WILDCARD_DOMAIN \
             -d password=$tenant_passwd \
-            $create_tenant_url > $output_dir/$orgName_tenant_signup.xml
+            $create_tenant_url > $output_dir/$output_file
 
-        # 2)  activate new user
+        # 2) Retrieve access_token
+        eval access_token=\"`xmlstarlet sel -t -m '//access_token' -v 'value' -n $output_dir/$output_file`\"
 
-        # 3) Create corresponding route
+
+        # 3)  activate new user
+        eval account_id=\"`xmlstarlet sel -t -m '//account' -v 'id' -n $output_dir/$output_file`\"
+        eval user_id=\"`xmlstarlet sel -t -m '///user[state = "pending"]' -v 'id' -n $output_dir/$output_file`\"
+        echo -en "\nactivating new user. account_id = $account_id. user_id = $user_id \n";
+        activate_user_url=https://$OCP_PROJECT_PREFIX-master-admin.$OCP_WILDCARD_DOMAIN/admin/api/accounts/$account_id/users/$user_id/activate.xml
+
+
+        echo -en "\n\n" >> $output_dir/$output_file
+        curl -k \
+             -X PUT \
+             -d access_token=$master_access_token \
+             $activate_user_url >> $output_dir/$output_file
+
+
+        # 4) Create corresponding route
         oc create route edge $orgName-provider --service=system-provider --hostname=$orgName-admin.$OCP_WILDCARD_DOMAIN
 
-        echo -en "\ncreated tenant with orgName= $orgName \n";
+        echo -en "\ncreated tenant with orgName= $orgName. \n\tOutput file at: $output_dir/$output_file  \n\taccess_token = $access_token \n";
+
+        echo -en "\n$userId   $access_token" >> $access_token_mapping_file
     done;
+
+    echo -en "\n\n\naccess_token_mapping_file available at: $access_token_mapping_file \n";
 
 }
 
